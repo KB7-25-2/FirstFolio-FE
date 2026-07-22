c87b0880075b5f13f3f57ea4fa4b55486934ab83
kchun0513 <62457974+kchun0513@users.noreply.github.com>
2026-07-22 17:50:29 +0900

    feat: 라우트 인증 가드 설정 #2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { setupAuthGuard } from '@/router/guards.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,8 +8,17 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('@/views/HomeView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue'),
+      meta: { guestOnly: true },
     },
   ],
 })
+
+setupAuthGuard(router)
 
 export default router
