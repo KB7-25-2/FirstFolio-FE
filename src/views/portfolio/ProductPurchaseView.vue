001c931920e713d146b5d90c29896a16c38f75a0
sjk911 <161575525+sjk911@users.noreply.github.com>
2026-08-20 09:44:10 +0900

    fix: 보유 목록에서 주식·펀드 현재가 미표시 문제 수정

--- src/views/portfolio/ProductPurchaseView.vue
+++ src/views/portfolio/ProductPurchaseView.vue
@@ -94,7 +94,10 @@ const closeTradeResult = () => {
 </script>
 
 <template>
-  <div class="flex flex-col gap-3">
+  <div
+    data-scroll-reveal-root
+    class="nav-scroll-pad flex min-h-0 flex-1 flex-col gap-3 overflow-y-auto overscroll-contain"
+  >
     <ScrollReveal>
       <div class="flex gap-2 overflow-x-auto pb-1">
         <button
